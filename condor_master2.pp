4c006d8f9c225f0db1d071f50a02bfd3937d5519
Trujillo, Fabian Andres (CIAT) <f.trujillo@cgiar.org>
2020-04-27 01:58:19 -0500

    Update condor_master2.pp

--- condor_master2.pp
+++ condor_master2.pp
@@ -4,9 +4,36 @@ class { 'ntp':
     servers => ['172.22.250.1']
   }
 
+apt:source { 'HTCondor':
+    comment => 'Repositorio de HTCondor',
+    location => 'http://research.cs.wisc.edu/htcondor/debian/stable/',
+    release  => 'unstable',
+    repos    => 'main contrib non-free',
+    pin      => '-10',
+    
+    key      => {
+    'id'     => 'A1BD8E9D78F7FE5C3E65D8AF8B48AD6246925553',
+    'server' => 'subkeys.pgp.net',
+  },
+  
+  include  => {
+    'src' => true,
+    'deb' => true,
+  },
+}
 
-package { 'tmuxinator':       
-    ensure => '0.7*', 
-    }
+
+package { 'HTCondor':
+    ensure => installed,
+    name   => "condor",
+}
+
+
+
+service {'Condor Service':
+    ensure => running,
+    name => "condor",
+}
+
 
 }
