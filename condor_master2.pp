4412bd0f7cd15431832a7810324d209313f70f84
Trujillo, Fabian Andres (CIAT) <f.trujillo@cgiar.org>
2020-04-29 13:14:20 -0500

    Update condor_master2.pp

--- condor_master2.pp
+++ condor_master2.pp
@@ -8,8 +8,8 @@ class { 'ntp':
 #Añadir repositorio de HTCondor
 
 file { "/etc/apt/source.list":
-    content => " deb [arch=amd64]  http://research.cs.wisc.edu/htcondor/ubuntu/8.8/bionic  wheezy contrib ", 
-    replace => false,
+    content => ' deb [arch=amd64]  http://research.cs.wisc.edu/htcondor/ubuntu/8.8/bionic  wheezy contrib ', 
+    replace => true,
 }
 
 
@@ -22,22 +22,22 @@ class { 'apt':
 
 
 #Recurso para instalar HTCondor
-package { 'htcondor':
-    ensure => '8.8',
-    name   => "condor",
-}
+#package { 'htcondor':
+#    ensure => '8.8',
+#    name   => "condor",
+#}
 
 #Recurso para iniciar el servicio de HTCondor
-service {'Condor Service':
-    ensure => running,
-    name => "condor",
-}
+#service {'Condor Service':
+#    ensure => running,
+#      name => "condor",
+#}
 
 #Recurso para modificar el archivo de configuracion de HTCONDOR
 
-file { '/etc/condor/condor_config.local':
-    content => "         \n", "           \n", 
-}
+#file { '/etc/condor/condor_config.local':
+#    content => "         \n", "           \n", 
+#}
 
 
 
